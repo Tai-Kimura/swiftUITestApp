e48a9a5cf715f6e353bc2294926aa472882e18c3
木村太一朗 <kimura@tanosys.com>
2025-08-04 11:03:56 +0900

    スクリプトミス

--- swiftUITestApp/clear_and_setup.sh
+++ swiftUITestApp/clear_and_setup.sh
@@ -7,14 +7,13 @@
   git reset --hard HEAD
   git clean -fd
 
-  cd "$SCRIPT_DIR/swiftUITestApp"
+  cd "$SCRIPT_DIR/bindingTestApp"
   rm -rf swiftui_builder
   cd ..
 
   # Download installer from GitHub
   echo "Downloading SwiftJsonUI installer..."
   curl -fsSL
-  https://raw.githubusercontent.com/Tai-Kimura/SwiftJsonUI/master/installer/bootstrap.sh | bash
-  -s -- -v 7.0.0-alpha -d ./bindingTestApp -m swiftui -s
+  https://raw.githubusercontent.com/Tai-Kimura/SwiftJsonUI/master/installer/bootstrap.sh | bash -s -- -v 7.0.0-alpha -d ./bindingTestApp -m swiftui -s
 
   cd "$SCRIPT_DIR/bindingTestApp/swiftui_builder/"
